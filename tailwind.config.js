0aea9a7d45de32b023daa74502242d0459153549
irdepla <rixsiboyevislombek@gmail.com>
2024-12-11 16:39:55 +0500

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
-      center: true, // Centers the containe
+      center: true, // Centers the container
       padding: "24px", // Adds padding to the container
       screens: {
         DEFAULT: "100%",
